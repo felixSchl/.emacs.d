5a22e70370d0154e5da90ab30829bcb4ab544a4e
Felix Schlitter <felix@sylo.io>
2018-11-23 10:43:13 +1300

    Configure C to use a tab width of 8

--- init.el
+++ init.el
@@ -508,8 +508,8 @@
  (progn
    (setq-default
     backward-delete-char-untabify-method nil
-    c-basic-offset 4
-    tab-width 4
+    c-basic-offset 8
+    tab-width 8
     indent-tabs-mode t)))
 
 ;; Markdown
